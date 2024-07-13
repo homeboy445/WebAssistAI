1951a01d1edc4bdadd4368916ee91103eb299531
homeboy445 <akshitsan13@gmail.com>
2024-07-13 18:54:15 +0530

    Added support for audio-based instructions

--- src/Utility/audioHandler.ts
+++ src/Utility/audioHandler.ts
@@ -1,4 +1,6 @@
+import { io } from "socket.io-client";
 import { getBaseURL } from "../consts/Urls";
+import axios from "axios";
 
 function blobToBase64(blob: Blob) {
   return new Promise((resolve, reject) => {
@@ -13,12 +15,15 @@ function blobToBase64(blob: Blob) {
   });
 }
 
+class AudioHandler {
+  
+  public readonly DEFAULT_AUDIO_TIMEOUT = 10000;
 
-export class AudioHandler {
-  mediaRecorder!: MediaRecorder;
-  audioBlobUrl = "";
-  recorderPromise: Promise<Blob> = Promise.resolve(new Blob());
-  autoStopTimer!: NodeJS.Timeout;
+  private mediaRecorder!: MediaRecorder;
+  private audioBlobUrl = "";
+  private recorderPromise: Promise<Blob> = Promise.resolve(new Blob());
+  private autoStopTimer!: NodeJS.Timeout;
+  public clearRecordingCompletionTimer: () => void = () => {};
 
   private waitForDOMReady() {
     return new Promise((resolve) => {
@@ -35,6 +40,9 @@ export class AudioHandler {
   async record() {
     await this.waitForDOMReady();
     let resolver = (value: Blob) => {};
+    if (this.mediaRecorder && this.mediaRecorder.state === "recording") {
+      throw new Error("already recording!");
+    }
     this.recorderPromise = new Promise((resolve) => {
       resolver = resolve;
     });
@@ -76,23 +84,70 @@ export class AudioHandler {
 
   async stop() {
     clearTimeout(this.autoStopTimer);
-    this.mediaRecorder.stop();
-    const blobData = await this.recorderPromise;
-    return {
-      sendToServer: async () => {
-        try {
-          const response = await fetch(`${getBaseURL()}/transcribe`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ base64File: await blobToBase64(blobData) }) // Convert the data object to a JSON string
+    try {
+      this.mediaRecorder.stop();
+      const blobData = await this.recorderPromise;
+      return {
+        sendToServer: async () => {
+          return new Promise((resolve) => {
+            try {
+            const reader = new FileReader();
+            reader.readAsArrayBuffer(blobData);
+            reader.onloadend = async () => {
+                debugger;
+                const arrayBuffer = reader.result;
+                const data = await axios.post(`${getBaseURL()}/transcribe`, arrayBuffer, {
+                  headers: {
+                    "Content-Type": "application/octet-stream",
+                  },
+                }).then((response) => {
+                  console.log("transcription response received!");
+                  if (response.status > 300) {
+                    throw new Error("error while sending audio to server");
+                  }
+                  return response.data;
+                }).catch(e => {
+                  console.log("error while sending audio to server: ", e);
+                  return { transcribedData: null };
+                });
+                if (!data.transcribedData) {
+                  resolve("error");
+                } else {
+                  resolve(data.transcribedData);
+                }
+            };
+            } catch (e) {
+              resolve("error");
+            }
           });
-          const data = await response.json();
-          console.log("received the transcribed data: ", data);
-        } catch (e) {}
+        },
+      };
+    } catch (e) {
+      return {
+        sendToServer: async () => "error"
       }
     }
+  }
+
+  async awaitCompletionAndTranscribe(timer?: number) {
+    this.clearRecordingCompletionTimer();
+    return new Promise((resolve) => {
+      const processRecording = async () => {
+        const options = await this.stop();
+        options.sendToServer()
+        .then(response => resolve(response))
+        .catch(e => resolve(e));
+      }
+      if (!timer) {
+        processRecording();
+      } else {
+        const timeout = setTimeout(processRecording, timer);
+        this.clearRecordingCompletionTimer = () => {
+          clearTimeout(timeout);
+          resolve(false);
+        }
+      }
+    });
   }
 
   reset() {
@@ -103,3 +158,6 @@ export class AudioHandler {
     return this.audioBlobUrl;
   }
 }
+
+const audioHandler = new AudioHandler();
+export { audioHandler };
--- src/popup.ts
+++ src/popup.ts
@@ -1,14 +1,20 @@
-// This file will control the popup.html file which is responsible for loading extension UI!
 import $ from "jquery";
-import { AITaskResponse, GenericObject, TaskType } from "./types/types";
+import {
+  AIResponseDialogBox,
+  audioRecorderInterface,
+  loader,
+} from "./components/components";
 
 const establishCommunication = async () => {
-  let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-  
-  const sendMessage = (messageObj: { type: string; task: string }, responseCb?: (response: unknown) => void) => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  const sendMessage = (
+    messageObj: { type: string; task: string },
+    responseCb?: (response: unknown) => void
+  ) => {
     chrome.tabs.sendMessage(tab.id || -1, messageObj, (response) => {
       if (!chrome.runtime.lastError) {
-        console.log('fine');
+        console.log("fine");
       } else {
         // This will print the mentioned error in the console
         console.log(chrome.runtime.lastError);
@@ -16,142 +22,143 @@ const establishCommunication = async () => {
       console.log("## received from contentScript: ", response);
       responseCb?.(response);
     });
-  }
-  console.log('established the connection!');
-  return {
-    sendMessage
   };
-}
+  console.log("established the connection!");
+  return {
+    sendMessage,
+  };
+};
 
 const globalPromiseManager = (() => {
-  let resolverCB = (...args: any) => {}, rejectorCB = () => {};
-  let promiseInstance = Promise.resolve();
-  return {
-    initialize: () => {
-      promiseInstance = new Promise((resolve, reject) => {
-        resolverCB = resolve;
-        rejectorCB = reject;
-      });
-    },
-    getPromise: () => promiseInstance,
-    resolver: (data: any) => {
-      resolverCB(data);
-    },
-    rejector: () => {
-      rejectorCB();
-    }
-  }
-})();
-
-const loader = (() => {
-  const loaderParent = $(".main-loader-section");
-  const loaderText = $("#loaderText");
-  const loaderBar = $("#loader");
-  const loaderTextList = [
-    "AI is curating the content for you!🤗",
-    "AI is learning from the data!🧐",
-    "Thinking...🤔",
-    "AI is working on it!🏃",
-    "AI is heavy at work!🤖",
-    "Understanding the page's data!📚",
-  ]
-  const updateText = (idx = 0) => {
-    loaderText.html(loaderTextList[(idx % loaderTextList.length)]);
-  }
-  const handler = {
-    run: (promiseToAwait: Promise<any>, timer = 100) => {
-      return new Promise((resolve) => {
-        loaderParent.css({ display: "flex" });
-        let width = 10;
-        let textIdx = 0;
-        let textChangingTimer: any;
-        const func = () => {
-          if (textChangingTimer === null) { return; }
-          const timer = Math.round(Math.random() * 5000);
-          textChangingTimer = setTimeout(func, timer);
-          updateText(textIdx++);
-        };
-        func();
-        const interval = setInterval(async () => {
-          width = Math.min(100, width + Math.round(Math.random() * 10));
-          if (width >= 100) {
-            clearInterval(interval);
-            resolve(await promiseToAwait);
-            clearInterval(textChangingTimer);
-            setTimeout(() => {
-              handler.reset();
-            }, 1000);
-          }
-          loaderBar.css({ width: `${width}%` });
-        }, timer);
-      });
-    },
-    reset: () => {
-      loaderBar.css({ width: "0%" });
-      loaderParent.css({ display: "none" });
-    }
+  const promiseHandlerCallbacks: {
+    [props: string]: { resolver: (...args: any) => void; rejector: () => void };
+  } = {};
+  const promiseStore: { [props: string]: Promise<any> } = {
+    global: Promise.resolve(),
   };
-  return handler;
-})();
-
-const AIResponseDialogBox = (() => {
-  const dialogBox = $(".main-ai-response");
-  const infoBox = $("#additionalInfo");
-  const operationStatusSection = $("#operationStatus");
-  $(".close-dialog-btn").on("click", () => {
-    dialogBox.css({ display: "none" });
-  });
   return {
-    show({ result, additionalInfo, operationSuccess, functionType }: AITaskResponse) {
-      let message = "AI's got nothing to say!😅";
-      let operationMessage = "The operation was a failure!🥲";
-      if (functionType === TaskType.DOM_OPERATION) {
-        message = additionalInfo || message;
-        operationMessage = "The requested DOM operation was a " + (operationSuccess ? "success🥳" : "failure🥲");
-      } else if (functionType === TaskType.INFO_RETRIEVAL) {
-        message = "The requested info is here!";
-        // TODO: Use additionalInfo here as well!
-        operationMessage = "The requested info retrieval operation was a " + (operationSuccess ? "success🥳" : "failure🥲");
-      }
-      if (result && typeof result === "object" && result.isError) {
-        message = "Something went wrong! Please try again! 😞"
-      }
-      infoBox.html(message);
-      operationStatusSection.html(operationMessage);
-      dialogBox.css({ display: "flex" });
+    initialize: (promiseAlias = "global") => {
+      return (promiseStore[promiseAlias] = new Promise((resolve, reject) => {
+        promiseHandlerCallbacks[promiseAlias] =
+          promiseHandlerCallbacks[promiseAlias] || {};
+        promiseHandlerCallbacks[promiseAlias].resolver = resolve;
+        promiseHandlerCallbacks[promiseAlias].rejector = reject;
+      }));
+    },
+    getPromise: (promiseAlias = "global") => promiseStore[promiseAlias],
+    resolver: (data: any, promiseAlias = "global") => {
+      promiseHandlerCallbacks[promiseAlias]?.resolver(data);
+    },
+    rejector: (promiseAlias = "global") => {
+      promiseHandlerCallbacks[promiseAlias]?.rejector();
     },
   };
 })();
 
 const initialize = async () => {
-  const element = document.querySelector(".search-submit");
-  element?.addEventListener("click", async () => {
+  const runAITask = async (task: string, staticMessage?: string) => {
+    const communicator = await establishCommunication();
+    globalPromiseManager.initialize();
+    communicator.sendMessage({
+      type: "WEB_TASK",
+      task: task.trim(),
+    });
+    const responsePromise = globalPromiseManager.getPromise();
+    const timer = setTimeout(() => {
+      globalPromiseManager.resolver({
+        operationSuccess: false,
+        result: "The operation timed out!😅",
+      });
+    }, 60000 * 5);
+    responsePromise.then((message: any) => {
+      clearTimeout(timer);
+      AIResponseDialogBox.show(message.data);
+    });
+    await loader.run(responsePromise, 100, staticMessage);
+  };
+
+  const handleSearchSubmit = async () => {
     const searchBar = document.querySelector(".search-bar") as HTMLInputElement;
     if (searchBar?.value) {
-      const communicator = await establishCommunication();
-      globalPromiseManager.initialize();
-      communicator.sendMessage({ type: 'WEB_TASK', task: searchBar.value.trim() });
-      const responsePromise = globalPromiseManager.getPromise();
-      const timer = setTimeout(() => {
-        globalPromiseManager.resolver({ operationSuccess: false, result: "The operation timed out!😅" });
-      }, 60000 * 5);
-      responsePromise.then((message: any) => {
-        clearTimeout(timer);
-        AIResponseDialogBox.show(message.data);
-      });
-      await loader.run(responsePromise);
+      runAITask(searchBar.value);
       searchBar.value = "";
     }
-  });
-  chrome.runtime.onMessage.addListener((message) => {
+  };
+
+  const handleRecordAudio = () => {
+    const handler = {
+      start: async () => {
+        const communicator = await establishCommunication();
+        communicator.sendMessage({
+          type: "WEB_RECORD_AUDIO_START",
+          task: "record",
+        });
+        const promiseInstance = globalPromiseManager.initialize("recorder");
+        audioRecorderInterface.show();
+        const timer = setTimeout(() => {
+          audioRecorderInterface.logError();
+          globalPromiseManager.resolver(
+            { operationSuccess: false, result: "The operation timed out!😅" },
+            "recorder"
+          );
+          audioRecorderInterface.hide();
+        }, 30000);
+        promiseInstance.then((response) => {
+          console.log("recording promise resolved! ", response);
+          clearTimeout(timer);
+          if (response.data && typeof response.data.transcribedData === "string" && response.data.transcribedData !== "error") {
+            runAITask(response.data.transcribedData, `Processing request: '${response.data.transcribedData}'`);
+            audioRecorderInterface.hide();
+          } else {
+            audioRecorderInterface.logError();
+            setTimeout(() => { audioRecorderInterface.hide() }, 1000);
+          }
+        });
+      },
+      stop: async (type: string) => {
+        console.log("stopping audio recording! ", type);
+        const communicator = await establishCommunication();
+        communicator.sendMessage({
+          type: "WEB_RECORD_AUDIO_STOP",
+          task: "record",
+        });
+      },
+    };
+    let audioRecordingStarted = false;
+    let timeout: NodeJS.Timeout | undefined;
+    if (!audioRecordingStarted) {
+      handler.start();
+      timeout = setTimeout(() => {
+        handler.stop("timer");
+        audioRecorderInterface.haltTimer();
+      }, 10000);
+      audioRecorderInterface.onStop(() => {
+        clearTimeout(timeout);
+        handler.stop("manual");
+      });
+      audioRecordingStarted = true;
+    } else {
+      clearTimeout(timeout);
+      handler.stop("toggled");
+    }
+  };
+
+  const handleChromeRuntimeMessage = (message: any) => {
     if (message.type === "WEB_TASK_RESPONSE") {
       globalPromiseManager.resolver(message);
+    } else if (message.type === "WEB_RECORD_AUDIO_RESPONSE") {
+      console.log("received audio blob url: ", message.data);
+      globalPromiseManager.resolver(message, "recorder");
     }
-  });
-}
+  };
 
-if (document.readyState !== 'loading') {
+  $(".search-submit").on("click", handleSearchSubmit);
+  $(".record-audio").on("click", handleRecordAudio);
+  chrome.runtime.onMessage.addListener(handleChromeRuntimeMessage);
+};
+
+if (document.readyState !== "loading") {
   initialize();
 } else {
-  document.addEventListener('DOMContentLoaded', initialize);
+  document.addEventListener("DOMContentLoaded", initialize);
 }
